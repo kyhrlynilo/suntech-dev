d675098f9d87b6a160645d4551c0555cfd96d0d1
kyhrlynilo <37626636+kyhrlynilo@users.noreply.github.com>
2023-07-17 05:38:11 +0800

    Delete SunTech.App directory

--- SunTech.App/SunTech.Infrastructure/Services/AzureEventGrid/IAzureEventGridService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace SunTech.Infrastructure.Services.AzureEventGrid
-{
-    public interface IAzureEventGridService
-    {
-        Task PublishCustomerEvent(string subject, string verb, dynamic jsonStr);
-    }
-}
--- SunTech.App/SunTech.Infrastructure/Services/CustomHttpClient/ICustomHttpClientService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace SunTech.Infrastructure.Services.CustomHttpClient
-{
-    public interface ICustomHttpClientService
-    {
-        Task SendMessageToEventGrid(string subject, string verb, dynamic data);
-    }
-}
